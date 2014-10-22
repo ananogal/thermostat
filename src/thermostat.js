288335fb994b4529703e8c3ad9848c833b4beafe
ananogal <ana.nogal@gmail.com>
2014-10-22 18:00:47 +0100

    refactoring thermostat

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,18 +1,18 @@
 function Thermostat(){
 	this.defaultTemperature= 20;
 	this.minTemperature = 10;
-	this.maxPowerSaverTemperature = 25;
-	this.maxTemperature = 32;
+	this.maxPowerSaverOnTemperature = 25;
+	this.maxPowerSaverOffTemperature = 32;
 	this.temperature = this.defaultTemperature;
-	this.isPowerSaverOn = true;	
-};
+	this.isPowerSaverOn = true;
+	this.maxTemperature = this._setMaxTemperature();	
+}
 
 Thermostat.prototype.increaseTemperature = function() {
 	return this.increaseTemperatureBy(1);
 };
 
 Thermostat.prototype.increaseTemperatureBy = function(degrees) {
-	if(this._isAboveMaxPowerSaverTemperature(degrees)) return this.temperature = this.maxPowerSaverTemperature;
 	if(this._isAboveMaxTemperature(degrees)) return this.temperature = this.maxTemperature;
 	
 	return this.temperature += degrees;
@@ -23,31 +23,35 @@ Thermostat.prototype.decreaseTemperature = function() {
 };
 
 Thermostat.prototype.decreaseTemperatureBy = function(degrees) {
-	if(this._isBellowMinTemperature(degrees)) return this.temperature = this.minTemperature;
+	if(this._isBelowMinTemperature(degrees)) return this.temperature = this.minTemperature;
 		 
 	return this.temperature -= degrees;
 };
 
 Thermostat.prototype.turnPowerSaverOff = function() {
 	this.isPowerSaverOn = false;
+	this._setMaxTemperature();
 };
 
 Thermostat.prototype.turnPowerSaverOn = function() {
 	this.isPowerSaverOn = true;
+	this._setMaxTemperature();
 };
 
 Thermostat.prototype.reset = function(){
 	return this.temperature = this.defaultTemperature;
 };
 
-Thermostat.prototype._isAboveMaxPowerSaverTemperature = function(degrees){
-	return (this.isPowerSaverOn) && (this.temperature + degrees > this.maxPowerSaverTemperature );
-};
-
 Thermostat.prototype._isAboveMaxTemperature = function(degrees){
-	return this.temperature + degrees > this.maxTemperature;
+	return (this.temperature + degrees) > this.maxTemperature;
 };
 
 Thermostat.prototype._isBelowMinTemperature = function(degrees){
-	return this.temperature - degrees < this.minTemperature;
+	return (this.temperature - degrees) < this.minTemperature;
 };
+
+Thermostat.prototype._setMaxTemperature = function(){
+	return this.maxTemperature = this.isPowerSaverOn ? this.maxPowerSaverOnTemperature : this.maxPowerSaverOffTemperature;
+};
+
+
