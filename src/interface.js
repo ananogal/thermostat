5cbe10da99a1b5c1645264be94f5f4a55c941efc
ananogal <ana.nogal@gmail.com>
2014-10-23 11:33:08 +0100

    start interface all over again. correct spelling errors in index

--- src/interface.js
+++ src/interface.js
@@ -1,22 +1,12 @@
-function ThermostatView(element){
-	this.el = $(element);
-	this.thermostat = new Thermostat();
-	this.el.text(this.thermostat.temperature);
-	this.coldTemperature = 18;
-	this.warmTemperature = 25;
-	this.bindTo('.increse_temperature', this.thermostat, this.thermostat.increaseTemperature);
-	this.bindTo('.decrese_temperature', this.thermostat, this.thermostat.decreaseTemperature);
-	this.bindTo('.reset', this.thermostat, this.thermostat.resetTemperature);
-};
+$(document).ready(function(){
+	var thermostat = new Thermostat();
+	$(".temperature h1").text(thermostat.temperature);
 
-ThermostatView.prototype.bindTo = function(selector, obj, func) {
-	$(selector).on('click', function(){
-		this.el.text(func.call(obj)).toggleClass(this.setColor());
+	$(".increase_temperature").on('click', function(){
+		$(".temperature h1").text(thermostat.increaseTemperature());
 	});
-};
 
-ThermostatView.prototype.setColor = function(){
-	if (parseInt(this.el.text) < this.coldTemperature) return "green"; 
-	if (parseInt(this.el.text) < this.warmTemperature) return "yellow"; 
-	return "red"; 
-} 
+	$(".decrease_temperature").on('click', function(){
+		$(".temperature h1").text(thermostat.decreaseTemperature());
+	});
+});
